eca3c9f2c17aeb9b95e190de1b379dbe2fcbfb4f
salgadop <paulasalgado3@gmail.com>
2019-06-14 17:12:58 -0300

    backup 14-07

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.get('/', function (req,res){
 	res.send (
-		"<html><head></head><body>Hola "+process.env.NOMBRE+"<script>document.body.style.backgroundColor='"+process.env.COLOR+"';</script></body></html>")
+		"<html><head></head><body>Hola "+process.env.NOMBRE+"!!! Puerto:"+process.env.PUERTO+"<script>document.body.style.backgroundColor='"+process.env.COLOR+"';</script></body></html>")
 })
 app.listen(process.env.PUERTO, function(){
 	console.log('app listening on '+process.env.PUERTO )
